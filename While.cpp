3b8ef665e3cf09e16642f7fb7680f114babc9f53
Arthur <arthurrapach06@gmail.com>
2022-05-26 18:35:29 -0300

    Create While.cpp

---
+++ While.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>>
+/*1) Faça uma algoritmo que leia valores enquanto  forem menores que 20;*/
+main ()
+{
+	int val=0;
+	do
+	{
+	printf("Informe um numero\n");
+	scanf("%d", &val);
+	}
+	while (val <=20);
+}
